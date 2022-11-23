2c3dd858a29dc6b8cfac611aa9a30838fded3b1f
Gikonyo-Otieno <gikonyootieno@gmail.com>
2022-11-23 14:37:02 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  
+  let response 
+  if (feet <= 400) {
+    response = 'This one is on me!'
+  }else if (feet > 2000) {
+    response = 'I will gladly take your thirty bucks.'
+  } if (feet > 2500 ){
+    response = "No can do."
+  }
+   
+  return response
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(destination){
+  return destination === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  
+
+   
+
+
+function switchOnCharmFromTip(Tip){
+  
+  let reply 
+  switch (Tip){
+    case 'generous':
+      reply = "Thank you so much.";
+      break;
+    case 'not as generous':
+      reply = "Thank you.";
+      break;
+    case 'thanks for everything' :
+      reply = "Bye.";    
+  }
+  return reply
 }
